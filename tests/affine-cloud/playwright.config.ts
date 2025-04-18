4dd4b9b814b41c5587015ad0fbec1b14cbd74da4
EYHN <cneyhn@gmail.com>
2025-04-18 14:36:12 +0800

    chore(ci): add url test to playwright

--- tests/affine-cloud/playwright.config.ts
+++ tests/affine-cloud/playwright.config.ts
@@ -32,12 +32,12 @@ const config: PlaywrightTestConfig = {
       //   we could download the builds from archives
       //   and then run the web with simple http serve, it's will be faster
       command: 'yarn run -T affine dev -p @affine/web',
-      port: 8080,
       timeout: 120 * 1000,
       reuseExistingServer: !process.env.CI,
       env: {
         COVERAGE: process.env.COVERAGE || 'false',
       },
+      url: 'http://localhost:8080',
     },
     {
       command: 'yarn run -T affine dev -p @affine/server',
@@ -59,6 +59,7 @@ const config: PlaywrightTestConfig = {
         MAILER_USER: 'noreply@toeverything.info',
         MAILER_PASSWORD: 'affine',
       },
+      url: 'http://localhost:3010',
     },
   ],
 };
