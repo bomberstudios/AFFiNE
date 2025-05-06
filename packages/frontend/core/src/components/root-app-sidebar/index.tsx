3ee0a8b74cda6cf037375899dac53719022c5d16
Jimmfly <yangjinfei001@gmail.com>
2025-05-06 15:04:05 +0800

    fix(core): go to workspace command does not work using CMDK

--- packages/frontend/core/src/components/root-app-sidebar/index.tsx
+++ packages/frontend/core/src/components/root-app-sidebar/index.tsx
@@ -103,9 +103,17 @@ export const RootAppSidebar = memo((): ReactElement => {
   const t = useI18n();
   const workspaceDialogService = useService(WorkspaceDialogService);
   const workbench = workbenchService.workbench;
+  const workspaceSelectorOpen = useLiveData(workbench.workspaceSelectorOpen$);
   const onOpenQuickSearchModal = useCallback(() => {
     cMDKQuickSearchService.toggle();
   }, [cMDKQuickSearchService]);
+
+  const onWorkspaceSelectorOpenChange = useCallback(
+    (open: boolean) => {
+      workbench.setWorkspaceSelectorOpen(open);
+    },
+    [workbench]
+  );
 
   const onOpenSettingModal = useCallback(() => {
     workspaceDialogService.open('setting', {
@@ -153,7 +161,12 @@ export const RootAppSidebar = memo((): ReactElement => {
       <SidebarContainer>
         <div className={workspaceAndUserWrapper}>
           <div className={workspaceWrapper}>
-            <WorkspaceNavigator showEnableCloudButton showSyncStatus />
+            <WorkspaceNavigator
+              showEnableCloudButton
+              showSyncStatus
+              open={workspaceSelectorOpen}
+              onOpenChange={onWorkspaceSelectorOpenChange}
+            />
           </div>
           <UserInfo />
         </div>
