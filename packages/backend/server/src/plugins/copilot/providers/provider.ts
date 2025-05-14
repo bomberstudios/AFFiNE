8762aaf93dbda143f2eb40146d0ac8a2e0f10541
DarkSky <darksky2048@gmail.com>
2025-05-14 15:11:19 +0800

    fix: null check

--- packages/backend/server/src/plugins/copilot/providers/provider.ts
+++ packages/backend/server/src/plugins/copilot/providers/provider.ts
@@ -256,7 +256,9 @@ export abstract class CopilotProvider<C = any> {
       if (
         'jsonMode' in options &&
         options.jsonMode &&
-        !messages.some(m => m.content.toLowerCase().includes('json'))
+        !messages.some(
+          m => m.content && m.content.toLowerCase().includes('json')
+        )
       ) {
         throw new CopilotPromptInvalid('Prompt not support json mode');
       }
